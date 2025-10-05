c502950b1127412e2ea032422f7eda3dac5c59f1
ward727 <Ward727a@gmail.com>
2025-10-05 05:48:38 +0200

    Some try with the modules system. Right now this is still in a very rough phase, as I just wanted to try and see how to do it, with security.
    Modules AREN'T plugins, they aren't sandboxed inside a VM, so it should be used with caution, they are dll .net 8 file, that will be run if their SHA256 checksum is added to the CHECKSUM_INTERNAL_MODULES inside EngineModules.cs class!
    
    Will it be kept? Probably, but with a lots of warning to the user if a non-trusted module is added (aka external module, that isn't inside the CHECKSUM_INTERNAL_MODULES).
    
    second commit because I'm an idiot that forgot to add the testModule source file

---
+++ modules/TestModule/TestModule.cs
@@ -0,0 +1,18 @@
+﻿using RPGCreator.Core.ModuleSDK;
+using Serilog;
+
+namespace TestModule;
+
+public class TestModule : IEngineModule
+{
+    public Version TargetEngineVersion { get; } = new Version(1, 0, 0);
+    public string Name { get; } = "Test Module";
+    public string Version { get; } = "1.0.0";
+    public string Author { get; } = "Your Name";
+    public string Description { get; } = "A test module for RPG Creator.";
+    public void Initialize()
+    {
+        // Initialization code here
+        Log.Information("Test Module initialized.");
+    }
+}
